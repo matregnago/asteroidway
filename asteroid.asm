4e0cd4b5b2b6846a23788d0b104ad4c0a9c23037
Matheus Tregnago <maugustotreg@gmail.com>
2023-11-22 21:46:52 -0300

    readme add

--- asteroid.asm
+++ asteroid.asm
@@ -1,13 +1,3 @@
-; TO DO
-; Tela de game over
-; Tela de ganhou
-; Tiro da nave
-; Spawn cura e shield
-; Niveis
-; Otimizar/mudar codigo (lodsw/stosw)
-; Aumentar numero asteroides
-; Controlar velocidade objetos
-
 .model small
 .stack 1000H
 .data 
---
+++ readme.md
@@ -0,0 +1,9 @@
+# TO DO
+- Tela de game over
+- Tela de ganhou
+- Tiro da nave
+- Spawn cura e shield
+- Niveis
+- Otimizar/mudar codigo (lodsw/stosw)
+- Aumentar numero asteroides
+- Controlar velocidade objetos
